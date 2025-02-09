d9aa40d71f45968998b2aac9d625ded132abf795
刘雨乘 <liuyucheng@Mac.lan>
2025-02-09 00:43:02 -0800

    Update Submit shell.

--- SubmitToGit.sh
+++ SubmitToGit.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+target_path="/Users/liuyucheng/Desktop/DSCI551/Project"
+current_path=$(pwd)
+
+if [ "$current_path" != "$target_path" ]; then
+  echo "当前目录 $current_path，不是目标目录，正在切换到 $target_path"
+  cd "$target_path" || { echo "❌ 无法切换到目标目录 $target_path"; exit 1; }
+fi
+
 # 检查是否提供了 commit message
 if [ -z "$1" ]; then
     echo "❌ 错误: 请提供 commit message!"
